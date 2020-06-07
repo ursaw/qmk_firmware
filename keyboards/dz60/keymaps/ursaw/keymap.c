ffb6b24bb4cd808eb4a91f209b19ade8bda8af91
urs <urs.w@gmx.de>
2020-06-07 18:21:32 +0200

    change DEL with ML(_FL)

--- keyboards/dz60/keymaps/ursaw/keymap.c
+++ keyboards/dz60/keymaps/ursaw/keymap.c
@@ -25,14 +25,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * ,-----------------------------------------------------------.
    */
 	[_BL] = LAYOUT(
-		KC_GESC, KC_1,   KC_2 , KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, KC_BSLS ,MO(_FK) ,
-		KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC,KC_BSPC ,
+		KC_GESC, KC_1,   KC_2 , KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, KC_BSLS ,KC_DEL ,
+		KC_TAB,     KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC,        KC_BSPC ,
 		MO(_FL), KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, LT(_ML,KC_SCLN), KC_QUOT, KC_ENT,
 		KC_LSFT, KC_NO, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, RSFT_T(KC_UP), MO(_FL),
 		KC_LCTL, KC_LGUI, KC_LALT, KC_SPC, KC_SPC, KC_SPC, KC_RALT, /*RGUI_T(*/KC_LEFT, KC_NO, KC_DOWN , RCTL_T(KC_RIGHT)
         ),
 
-    // no idea yet on which key to put TG(_FK) - toggles layer, activating it if it’s inactive and vice versa
+    // no idea yet on which key to put TG(_FK) or MO(_FK)  - toggles layer, activating it if it’s inactive and vice versa
     [_FK] = LAYOUT(
 		KC_ESC, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, _______, _______,
 		_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______ ,
@@ -49,18 +49,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |-----------------------------------------------------------|
    * |   MO  | LE| DN| RE|   |   | LE| DN| UP| RE|   |   |Return |
    * |-----------------------------------------------------------|
-   * |Shift   |   |   |   |   |   |   |MUT|MUP|MDN|  \|  PUP | MO|
+   * |Shift   |   |   |MENU|  |   |   |MUT|MUP|MDN|  \|  PUP | MO|
    * |-----------------------------------------------------------|
    * |Ctrl|Gui |Alt |      Space            |ALT| HOME |PDN |END |
    * ,-----------------------------------------------------------.
    */
 
 	[_FL] = LAYOUT(
-		KC_GRV, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_INS, _______,
+		KC_GRV, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, _______, KC_INS,
 		KC_CAPS, XXXXXXX, KC_UP,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_MPLY, KC_MPRV, KC_MNXT, KC_DEL ,
 		            /* WASD for cursor */                        /*  VI for cursor  */
         _______, KC_LEFT, KC_DOWN, KC_RIGHT,XXXXXXX, XXXXXXX, KC_LEFT, KC_DOWN, KC_UP  , KC_RIGHT, _______, _______, _______,
-		_______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_MUTE, KC_VOLD, KC_VOLU, KC_BSLS, KC_PGUP, _______,
+		_______, XXXXXXX, XXXXXXX, XXXXXXX, KC_APP , XXXXXXX, XXXXXXX, XXXXXXX, KC_MUTE, KC_VOLD, KC_VOLU, KC_BSLS, KC_PGUP, _______,
 		_______, _______, _______, _______, _______, _______, _______, KC_HOME, _______,            KC_PGDOWN, KC_END
         ),
    /* Keymap: (Mouse Layer) incl Macros TODO
