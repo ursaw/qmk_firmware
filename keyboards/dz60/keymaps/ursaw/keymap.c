6cbb2fb25235c2fbab1dc947f634ac5263f06099
urs <urs.w@gmx.de>
2020-09-23 08:47:01 +0200

    deactivare z, x as ctlr, super

--- keyboards/dz60/keymaps/ursaw/keymap.c
+++ keyboards/dz60/keymaps/ursaw/keymap.c
@@ -28,7 +28,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		KC_GESC, KC_1,   KC_2 , KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, KC_BSLS ,KC_DEL ,
 		KC_TAB,     KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC,        KC_BSPC ,
 		MO(_FL), KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, LT(_ML,KC_SCLN), KC_QUOT, KC_ENT,
-		KC_LSFT, KC_NO, LCTL_T(KC_Z), LGUI_T(KC_X), KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, RCTL_T(KC_SLSH), RSFT_T(KC_UP), MO(_FL),
+		KC_LSFT, KC_NO, /*LCTL_T(*/KC_Z, /*LGUI_T()*/KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, /*RCTL_T(*/KC_SLSH, RSFT_T(KC_UP), MO(_FL),
 		KC_LCTL, KC_LGUI, KC_LALT, KC_SPC, KC_SPC, KC_SPC, KC_RALT, /*RGUI_T(*/KC_LEFT, KC_NO, KC_DOWN , RCTL_T(KC_RIGHT)
         ),
 
@@ -61,7 +61,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                     /* WASD for cursor */                    /* - - - -  VI for cursor  - - - - */ /* _ML */
         _______, KC_LEFT, KC_DOWN, KC_RIGHT,XXXXXXX, XXXXXXX, KC_LEFT, KC_DOWN, KC_UP  , KC_RIGHT, _______, KC_GRAVE, _______,
         _______, XXXXXXX, XXXXXXX, KC_LGUI , KC_APP , XXXXXXX, XXXXXXX, XXXXXXX, KC_MUTE, KC_VOLD, KC_VOLU, KC_BSLS, KC_PGUP, _______,
-        _______, _______, _______, _______, _______, _______, _______, KC_HOME, _______,            KC_PGDOWN, KC_END
+        _______, _______, _______, _______, _______, _______, KC_LGUI, KC_HOME, _______,            KC_PGDOWN, KC_END
         ),
    /* Keymap: (Mouse Layer) incl Macros TODO
    * ,-----------------------------------------------------------.
