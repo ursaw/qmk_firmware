c368c5f7e9c72a87922b2254c61b966ae7ec7be7
urs <urs.w@gmx.de>
2020-11-10 21:46:29 +0100

    adding DYNAMIC_MACROS, played with KC_LEAD

--- keyboards/keebio/quefrency/keymaps/ursaw/keymap.c
+++ keyboards/keebio/quefrency/keymaps/ursaw/keymap.c
@@ -87,7 +87,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * ,-----------------------------------------------------------.
    */
   [_ML] = LAYOUT(
-    KC_TRNS, M(1)   , M(2)   , M(3)   , M(4)   , M(5)   , M(6)   , M(7)   , M(8)   , M(9)   , M(10)  , M(11)  , M(12)  , XXXXXXX, RESET   , \
+  //  KC_TRNS, M(1)   , M(2)   , M(3)   , M(4)   , M(5)   , M(6)   , M(7)   , M(8)   , M(9)   , M(10)  , M(11)  , M(12)  , XXXXXXX, RESET   ,
+    KC_TRNS, DYN_MACRO_PLAY1   , DYN_MACRO_PLAY2   , M(3)   , M(4)   , M(5)   , M(6)   ,DYN_REC_START1   , DYN_REC_START2  , XXXXXXX  ,DYN_REC_STOP  ,XXXXXXX  , M(12)  , XXXXXXX, RESET   , \
    	XXXXXXX,  KC_MS_BTN1, KC_MS_UP,  KC_MS_BTN2,   XXXXXXX, RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD, _______, RGB_VAD, RGB_MODE_PLAIN, \
 	_______,  KC_MS_LEFT, KC_MS_DOWN,KC_MS_RIGHT,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______, _______, _______, \
     XXXXXXX, XXXXXXX, KC_MS_ACCEL0, KC_MS_ACCEL1, KC_MS_ACCEL2,  XXXXXXX, BL_DEC,  BL_TOGG, BL_INC,  BL_STEP, XXXXXXX, _______, _______, \
@@ -96,4 +97,60 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 };
 
 
+/*
+ rules.mk: LEADER_ENABLE = yes
+comfig.h: #define LEADER_TIMEOUT 300
+define KC_LEAD somewhere
 
+
+LEADER_EXTERNS();
+
+// Declare a boolean variable to keep track of whether any sequence
+// will have been matched.
+bool did_leader_succeed;
+
+void matrix_scan_user(void) {
+  LEADER_DICTIONARY() {
+    // Initialize did_leader_succeed as well as leading to be false
+    did_leader_succeed = leading = false;
+
+    // Replace the sequences below with your own sequences.
+    SEQ_ONE_KEY(KC_A) {
+      SEND_STRING(SS_LCTRL(SS_LSFT("t")));
+      // In each sequence, set our flag to true. This way, we'll
+      // know when any sequence was matched.
+      did_leader_succeed = true;
+    }
+    SEQ_ONE_KEY(KC_T) {
+      SEND_STRING(SS_LCTRL(SS_LSFT("t")));
+      // In each sequence, set our flag to true. This way, we'll
+      // know when any sequence was matched.
+      did_leader_succeed = true;
+    }
+    SEQ_TWO_KEYS(KC_N, KC_T) {
+      SEND_STRING(SS_LCTRL("t"));
+      did_leader_succeed = true;
+    }
+    //SEQ_TWO_KEYS(KC_C, KC_C) {
+    //  SEND_STRING("Per key timing is great!!!");
+    //}
+
+    // Call leader_end at the end of the function, instead of at
+    // the start. This way, we're sure we have set did_leader_succeed.
+    leader_end();
+  }
+}
+
+void leader_end(void) {
+  if (did_leader_succeed) {
+    // If any sequence was matched, did_leader_succeed will have
+    // been set to true up in the matrix_scan_user function.
+    // Put your code for a matched leader key sequence here.
+  } else {
+    // If no sequence was matched, did_leader_succeed will not
+    // have been set to true anywhere, so we'll end up here.
+    // Put your code for an unmatched leader key sequence here.
+  }
+}
+
+*/
