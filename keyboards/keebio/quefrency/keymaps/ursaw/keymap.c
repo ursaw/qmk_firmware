9289e3a53bffbed4e2ff7fc467d8f6311517327b
urs <urs.w@gmx.de>
2020-11-10 20:58:41 +0100

    adding mouselayer

--- keyboards/keebio/quefrency/keymaps/ursaw/keymap.c
+++ keyboards/keebio/quefrency/keymaps/ursaw/keymap.c
@@ -22,25 +22,25 @@ enum custom_keycodes {
   QWERTY = SAFE_RANGE,
 };
   /* Keymap: (Base Layer) Default Layer
-   * ,-----------------------------------------------------------.
-   * |Esc~| 1|  2|  3|  4|  5|  6|  7|  8|  9|  0|  -|  =|  \| MO|
-   * |-----------------------------------------------------------|
-   * |Tab  |  Q|  W|  E|  R|  T|  Y|  U|  I|  O|  P|  [|  ]|BCKSP|
-   * |-----------------------------------------------------------|
-   * |   MO  |  A|  S|  D|  F|  G|  H|  J|  K|  L|  ;|  '|Return |
-   * |-----------------------------------------------------------|
-   * |Shift   |  Z|  X|  C|  V|  B|  N|  M|  ,|  .|  /|Shift | MO|
-   * |-----------------------------------------------------------|
-   * |Ctrl|Gui |Alt |      Space            |Alt  | LE| DN|RE/CTR|
-   * ,-----------------------------------------------------------.
+   * ,---------------------------.      ,-------------------------------.
+   * |Esc~| 1|  2|  3|  4|  5|  6|      |  7|  8|  9|  0|  -|  =|  \| MO|
+   * |---------------------------'    ,---------------------------------|
+   * |Tab  |  Q|  W|  E|  R|  T|      |  Y|  U|  I|  O|  P|  [|  ]|BCKSP|
+   * |---------------------------.    '---------------------------------|
+   * |   _FL  |  A|  S|  D|  F|  G|      |  H|  J|  K|  L|  ;|  '|Return |
+   * |----------------------------.     '-------------------------------|
+   * |Shift   |  Z|  X|  C|  V|  B|      |  N|  M|  ,|  .|  /|Shift | MO|
+   * |----------------------------|      |------------------------------|
+   * |Ctrl|Gui |Alt |      Space  |      |         |Alt  | LE| DN|RE/CTR|
+   * ,----------------------------'      '------------------------------'
    */
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_BL] = LAYOUT(
     KC_GESC, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL, KC_BSLS ,KC_DEL , \
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSPC, \
-    MO(_FL), KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,   /*LT(_ML,*/ KC_SCLN, KC_QUOT, KC_ENT, \
+    MO(_FL), KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L, LT(_ML,KC_SCLN), KC_QUOT, KC_ENT, \
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, RSFT_T(KC_UP), MO(_FL), \
-    KC_LCTL, KC_LGUI, MO(_FL), KC_LALT,  KC_SPC,          KC_BSPC,KC_SPC, KC_RALT, KC_LEFT, KC_NO, KC_DOWN, RCTL_T(KC_RIGHT)
+    KC_LCTL, KC_LGUI, KC_LALT, TG(_FK),  KC_SPC,          KC_NO,  KC_SPC, KC_RALT, KC_LEFT, KC_NO, KC_DOWN, RCTL_T(KC_RIGHT)
   ),
    /* Keymap: (Functional Layer)
    * ,-----------------------------------------------------------.
@@ -50,17 +50,50 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |-----------------------------------------------------------|
    * |   MO  | LE| DN| RE|   |   | LE| DN| UP| RE| _ML | ` |Rtrn |
    * |-----------------------------------------------------------|
-   * |Shift   |   |   |MENU|  |   |   |MUT|MUP|MDN|  \|  PUP | MO|
+   * |Shift   |   |WIN|MENU|  |   |   |MUT|MUP|MDN|  \|  PUP | MO|
    * |-----------------------------------------------------------|
    * |Ctrl|Gui |Alt |      Space            |ALT| HOME |PDN |END |
    * ,-----------------------------------------------------------.
    */
   [_FL] = LAYOUT(
     KC_GESC, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_TILDE,  KC_INS, \
-    KC_CAPS, RGB_MOD, _______, KC_UP,   _______, _______, _______, _______, _______, _______, KC_MPLY, KC_MPRV, KC_MNXT, KC_DEL, \
+    KC_CAPS, _______, KC_UP, _______,   _______, _______, _______, _______, _______, _______, KC_MPLY, KC_MPRV, KC_MNXT, KC_DEL, \
                         /* WASD for cursor */                    /* - - - -  VI for cursor  - - - - */ /* _ML */
-    _______, _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______, KC_LEFT, KC_DOWN, KC_UP  , KC_RIGHT ,_______, KC_GRAVE,\
+    _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______, _______, KC_LEFT, KC_DOWN, KC_UP  , KC_RIGHT ,_______, KC_GRAVE,\
     _______, _______, KC_LGUI, KC_APP , _______, _______, _______, KC_MUTE, KC_VOLD, KC_VOLU, KC_BSLS, KC_PGUP, _______, \
     _______, _______, _______, _______, _______,          _______, _______, _______, KC_HOME,  KC_NO , KC_PGDOWN, KC_END
   ),
+    /*
+     *  Funktional keys
+     */
+  [_FK] = LAYOUT(
+    KC_GESC, KC_F1  , KC_F2  , KC_F3  , KC_F4  , KC_F5  , KC_F6  , KC_F7  , KC_F8  , KC_F9  , KC_F10 , KC_F11 , KC_F12 , _______,  _______, \
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
+    _______, _______, _______, _______, _______,          _______, _______, _______, _______, _______, _______, _______
+  ),
+   /* Keymap: (Mouse Layer) incl Macros TODO
+   * ,-----------------------------------------------------------.
+   * |Esc~|M1| M2| M3| M4| M5| M6| M7| M8| M9|M10|<F>11|F12| |RST|
+   * |-----------------------------------------------------------|
+   * |     |  L| UP|  R|   |   |   |   |   |   |PLY|PRV|NXT| DEL |
+   * |-----------------------------------------------------------|
+   * |   MO  | LE| DN| RE|   |   |   |   |   |   |   |   |Return |
+   * |-----------------------------------------------------------|
+   * |Shift   |   |   |   |   |   |   |MUT|MUP|MDN|  \|  PUP | MO|
+   * |-----------------------------------------------------------|
+   * |Ctrl|Gui |Alt |      Space            |ALT| HOME |PDN |END |
+   * ,-----------------------------------------------------------.
+   */
+  [_ML] = LAYOUT(
+    KC_TRNS, M(1)   , M(2)   , M(3)   , M(4)   , M(5)   , M(6)   , M(7)   , M(8)   , M(9)   , M(10)  , M(11)  , M(12)  , XXXXXXX, RESET   , \
+   	XXXXXXX,  KC_MS_BTN1, KC_MS_UP,  KC_MS_BTN2,   XXXXXXX, RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD, _______, RGB_VAD, RGB_MODE_PLAIN, \
+	_______,  KC_MS_LEFT, KC_MS_DOWN,KC_MS_RIGHT,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______, _______, _______, \
+    XXXXXXX, XXXXXXX, KC_MS_ACCEL0, KC_MS_ACCEL1, KC_MS_ACCEL2,  XXXXXXX, BL_DEC,  BL_TOGG, BL_INC,  BL_STEP, XXXXXXX, _______, _______, \
+    _______, _______, _______, _______, _______,          _______, _______, _______, _______, _______, _______, _______
+  )
 };
+
+
+
