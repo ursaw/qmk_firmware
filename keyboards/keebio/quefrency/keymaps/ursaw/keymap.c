5f663932eddc955b09e3beb4927f13a45470c762
urs <urs.w@gmx.de>
2020-11-11 20:56:11 +0100

    slight cleanup

--- keyboards/keebio/quefrency/keymaps/ursaw/keymap.c
+++ keyboards/keebio/quefrency/keymaps/ursaw/keymap.c
@@ -27,11 +27,11 @@ enum custom_keycodes {
    * |---------------------------'    ,---------------------------------|
    * |Tab  |  Q|  W|  E|  R|  T|      |  Y|  U|  I|  O|  P|  [|  ]|BCKSP|
    * |---------------------------.    '---------------------------------|
-   * |   _FL  |  A|  S|  D|  F|  G|      |  H|  J|  K|  L|  ;|  '|Return |
+   * |   _FL  |  A|  S|  D|  F|  G|      |  H|  J|  K|  L|  ;|  '|Return|
    * |----------------------------.     '-------------------------------|
    * |Shift   |  Z|  X|  C|  V|  B|      |  N|  M|  ,|  .|  /|Shift | MO|
    * |----------------------------|      |------------------------------|
-   * |Ctrl|Gui |Alt |      Space  |      |         |Alt  | LE| DN|RE/CTR|
+   * |Ctrl|Gui |Alt |Alt | Return |      | SPACE   |Alt  | LE| DN|RE/CTR|
    * ,----------------------------'      '------------------------------'
    */
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -40,7 +40,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSPC, \
     MO(_FL), KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L, LT(_ML,KC_SCLN), KC_QUOT, KC_ENT, \
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, RSFT_T(KC_UP), MO(_FL), \
-    KC_LCTL, KC_LGUI, KC_LALT, TG(_FK),  KC_SPC,          KC_NO,  KC_SPC, KC_RALT, KC_LEFT, KC_NO, KC_DOWN, RCTL_T(KC_RIGHT)
+    KC_LCTL, KC_LGUI, KC_LALT,   KC_LALT, KC_ENT,         KC_NO,  KC_SPC, KC_RALT, KC_LEFT, KC_NO, KC_DOWN, RCTL_T(KC_RIGHT)
   ),
    /* Keymap: (Functional Layer)
    * ,-----------------------------------------------------------.
@@ -57,11 +57,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    */
   [_FL] = LAYOUT(
     KC_GESC, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_TILDE,  KC_INS, \
-    KC_CAPS, _______, KC_UP, _______,   _______, _______, _______, _______, _______, _______, KC_MPLY, KC_MPRV, KC_MNXT, KC_DEL, \
+    _______, _______, KC_UP, _______,   _______, _______, _______, _______, _______, _______, KC_MPLY, KC_MPRV, KC_MNXT, KC_DEL, \
                         /* WASD for cursor */                    /* - - - -  VI for cursor  - - - - */ /* _ML */
-    _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______, _______, KC_LEFT, KC_DOWN, KC_UP  , KC_RIGHT ,_______, KC_GRAVE,\
-    _______, _______, KC_LGUI, KC_APP , _______, _______, _______, KC_MUTE, KC_VOLD, KC_VOLU, KC_BSLS, KC_PGUP, _______, \
-    _______, _______, _______, _______, _______,          _______, _______, _______, KC_HOME,  KC_NO , KC_PGDOWN, KC_END
+    _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______, _______, KC_LEFT, KC_DOWN, KC_UP  , KC_RIGHT , KC_GRAVE ,_______,\
+    _______, _______, KC_LGUI, KC_APP , _______, _______, _______, KC_MUTE, KC_VOLD, KC_VOLU, KC_BSLS  , KC_PGUP, _______, \
+    _______, _______, _______, _______, _______,          _______, _______, _______, KC_HOME,  KC_NO   , KC_PGDOWN, KC_END
   ),
     /*
      *  Funktional keys
@@ -89,8 +89,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_ML] = LAYOUT(
   //  KC_TRNS, M(1)   , M(2)   , M(3)   , M(4)   , M(5)   , M(6)   , M(7)   , M(8)   , M(9)   , M(10)  , M(11)  , M(12)  , XXXXXXX, RESET   ,
     KC_TRNS, DYN_MACRO_PLAY1   , DYN_MACRO_PLAY2   , M(3)   , M(4)   , M(5)   , M(6)   ,DYN_REC_START1   , DYN_REC_START2  , XXXXXXX  ,DYN_REC_STOP  ,XXXXXXX  , M(12)  , XXXXXXX, RESET   , \
-   	XXXXXXX,  KC_MS_BTN1, KC_MS_UP,  KC_MS_BTN2,   XXXXXXX, RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD, _______, RGB_VAD, RGB_MODE_PLAIN, \
-	_______,  KC_MS_LEFT, KC_MS_DOWN,KC_MS_RIGHT,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______, _______, _______, \
+   	KC_CAPS,  KC_MS_BTN1, KC_MS_UP,  KC_MS_BTN2,   XXXXXXX, RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD, _______, RGB_VAD, RGB_MODE_PLAIN, \
+	_______,  KC_MS_LEFT, KC_MS_DOWN,KC_MS_RIGHT,   TG(_FK), XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______, _______, _______, \
     XXXXXXX, XXXXXXX, KC_MS_ACCEL0, KC_MS_ACCEL1, KC_MS_ACCEL2,  XXXXXXX, BL_DEC,  BL_TOGG, BL_INC,  BL_STEP, XXXXXXX, _______, _______, \
     _______, _______, _______, _______, _______,          _______, _______, _______, _______, _______, _______, _______
   )
