df18afd8246e4b55a66dec7ce6c4e805bb0e284a
urs <urs.w@gmx.de>
2021-02-05 20:56:56 +0100

    CTRL<>ALT

--- keyboards/sofle/keymaps/ursaw/keymap.c
+++ keyboards/sofle/keymaps/ursaw/keymap.c
@@ -51,11 +51,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
  * | TAB  |   Q  |   W  |   E  |   R  |   T  |                    |   Y  |   U  |   I  |   O  |   P  | Bspc |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * | CMTR |   A  |   S  |   D  |   F  |   G  |-------.    ,-------|   H  |   J  |   K  |   L  |   ;  |  '   |
+ * | LCTR |   A  |   S  |   D  |   F  |   G  |-------.    ,-------|   H  |   J  |   K  |   L  |   ;  |  '   |
  * |------+------+------+------+------+------|       |    |       |------+------+------+------+------+------|
  * |LShift|   Z  |   X  |   C  |   V  |   B  |-------|    |-------|   N  |   M  |   ,  |   .  |   /  |RShift|
  * `-----------------------------------------/       /     \      \-----------------------------------------'
- *            | LGUI | LAlt | LCTR |LOWER | /Enter  /       \Space \  |RAISE | RCTR | RAlt | RGUI |
+ *            | LGUI | LCTR | LAlt |LOWER | /Enter  /       \Space \  |RAISE | RAlt | RCTR | RGUI |
  *            |      |      |      |      |/       /         \      \ |      |      |      |      |
  *            `----------------------------------'           '------''---------------------------'
  */
@@ -65,7 +65,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,  KC_BSPC, \
   KC_LSFT, KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN,  KC_QUOT, \
   KC_LCTRL,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_MUTE,     XXXXXXX,KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_RSFT, \
-                 KC_LGUI,KC_LALT,KC_LCTRL, KC_RAISE, KC_ENT,      KC_SPC,  KC_RAISE, KC_RCTRL, KC_RALT, KC_APP \
+                 KC_LGUI,KC_LCTRL,KC_LALT, KC_RAISE, KC_ENT,      KC_SPC,  KC_RAISE, KC_RALT, KC_RCTRL, KC_APP \
 ),
 /*
  * COLEMAK
@@ -402,9 +402,9 @@ void encoder_update_user(uint8_t index, bool clockwise) {
         }
     } else if (index == 1) {
         if (clockwise) {
-            tap_code(KC_MS_WH_DOWN);  // KC_PGDOWN
+            tap_code(KC_PGDOWN);  // KC_MS_WH_DOWN
         } else {
-            tap_code(KC_MS_WH_UP);  // KC_PGUP
+            tap_code(KC_PGUP);    // KC_MS_WH_UP
         }
     }
 }
