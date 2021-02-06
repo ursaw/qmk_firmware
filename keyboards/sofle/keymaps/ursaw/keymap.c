903521cca3ca4ab494cea950b09270e5599d300d
urs <urs.w@gmx.de>
2021-02-06 15:43:33 +0100

    special keys to left

--- keyboards/sofle/keymaps/ursaw/keymap.c
+++ keyboards/sofle/keymaps/ursaw/keymap.c
@@ -65,7 +65,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,  KC_BSPC, \
   KC_LSFT, KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN,  KC_QUOT, \
   KC_LCTRL,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_MUTE,     XXXXXXX,KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_RSFT, \
-                 KC_LGUI,KC_LCTRL,KC_LALT, KC_RAISE, KC_ENT,      KC_SPC,  KC_RAISE, KC_RALT, KC_RCTRL, KC_APP \
+                 KC_LGUI,KC_LCTRL,KC_LALT, KC_RAISE, KC_ENT,      /*LT(_RAISE,*/KC_SPC,  KC_RAISE, KC_RALT, KC_RCTRL, KC_APP \
 ),
 /*
  * COLEMAK
@@ -127,10 +127,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *            `----------------------------------'           '------''---------------------------'
  */
 [_RAISE] = LAYOUT( \
-   KC_GRV,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                           KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F11,\
-  _______,  XXXXXXX,  KC_LGUI,   KC_APP,  XXXXXXX, XXXXXXX,                       KC_PGUP, KC_HOME,  KC_UP, KC_END   ,XXXXXXX, KC_F12, \
-  _______, XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX, KC_CAPS,                       KC_PGDN,  KC_LEFT, KC_DOWN, KC_RGHT,  KC_MINUS, KC_EQUAL, \
-  _______, KC_UNDO, KC_CUT, KC_COPY, KC_PASTE, XXXXXXX,  _______,         _______, KC_DEL, KC_BSPC, KC_LBRC, KC_RBRC,   KC_BSLASH, _______, \
+  _______,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                           KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F11,\
+  _______,  KC_PIPE,  KC_LCBR,   KC_RCBR,  KC_PLUS, KC_GRV,                       KC_PGUP, KC_HOME,  KC_UP, KC_END   ,XXXXXXX, KC_F12, \
+  _______, KC_GRV,  KC_LBRC, KC_RBRC, KC_MINUS, KC_EQUAL,                       KC_PGDN,  KC_LEFT, KC_DOWN, KC_RGHT,  KC_MINUS, KC_EQUAL, \
+  _______, KC_UNDO, XXXXXXX, KC_APP, KC_SPC, KC_UNDS,  _______,         _______, KC_DEL, KC_BSPC, KC_LBRC, KC_RBRC,   KC_BSLASH, _______, \
                          _______, _______, _______, _______, _______,       _______, _______, _______, _______, _______ \
 ),
 /* ADJUST
