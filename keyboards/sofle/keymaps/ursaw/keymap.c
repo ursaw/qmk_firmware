dec10590a5c38cf0fe0e3127f94cad5203201270
urs <urs.w@gmx.de>
2021-02-06 15:43:33 +0100

    special keys to left

--- keyboards/sofle/keymaps/ursaw/keymap.c
+++ keyboards/sofle/keymaps/ursaw/keymap.c
@@ -65,7 +65,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,  KC_BSPC, \
   KC_LSFT, KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN,  KC_QUOT, \
   KC_LCTRL,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_MUTE,     XXXXXXX,KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_RSFT, \
-                 KC_LGUI,KC_LCTRL,KC_LALT, KC_RAISE, KC_ENT,      KC_SPC,  KC_RAISE, KC_RALT, KC_RCTRL, KC_APP \
+                 KC_LGUI,KC_LCTRL,KC_LALT, KC_RAISE, KC_ENT,      /*LT(_RAISE,*/KC_SPC,  KC_RAISE, KC_RALT, KC_RCTRL, KC_APP \
 ),
 /*
  * COLEMAK
