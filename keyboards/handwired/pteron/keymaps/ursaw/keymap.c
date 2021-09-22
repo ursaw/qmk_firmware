cf011e17783fa979560f8ca34787ebe965bf8a2d
urs <urs.w@gmx.de>
2021-09-22 20:38:51 +0200

    ursaw pteron

--- keyboards/handwired/pteron/keymaps/ursaw/keymap.c
+++ keyboards/handwired/pteron/keymaps/ursaw/keymap.c
@@ -6,7 +6,7 @@
 //  PINS  https://golem.hu/article/pro-micro-pinout/
 //  TEST  https://config.qmk.fm/#/test
 
-// cd keyboards/handwired/pteron/keymaps/default
+// cd keyboards/handwired/pteron/keymaps/ursaw
 // qmk compile
 // qmk flash
 
@@ -38,11 +38,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *               +---------------------------+      +---------------------------+
  */
 [_QWERTY] = LAYOUT( \
-  KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,            KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC, \
-  KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,            KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_DEL,  \
-  KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,            KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
-  KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,            KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT,  \
-                    LOWER,   KC_SPC,  KC_LALT, KC_LGUI,         KC_RALT, KC_RGUI, KC_SPC,  RAISE \
+  KC_GESC,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,  KC_DEL, \
+  KC_TAB,    KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,  KC_BSPC, \
+  KC_LSFT,   KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN,  KC_QUOT, \
+  KC_LCTRL,  KC_Z, MT(MOD_LGUI,KC_X),    KC_C,    KC_V,    KC_B,                     KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_RSFT, \
+                    KC_LALT, RAISE,   KC_ENT,  XXXXXXX,        KC_SPC , KC_SPC, RAISE, KC_RALT \
 ),
 
 /* Lower
@@ -80,11 +80,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *               +---------------------------+      +---------------------------+
  */
 [_RAISE] = LAYOUT( \
-  KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,            KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC, \
-  KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,            KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL,  \
-  KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,           KC_F6,   KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS, \
-  _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,          KC_F12,  KC_NUHS, KC_NUBS, KC_PGUP, KC_PGDN, _______, \
-                    _______, _______, _______, _______,         KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY  \
+  _______,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                           KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F11,\
+  _______,  KC_PIPE,  KC_LCBR,   KC_RCBR,  KC_PLUS, KC_GRV,                       KC_PGUP, KC_HOME,  KC_UP, KC_END   ,XXXXXXX, KC_F12, \
+  _______, KC_GRV,  KC_LBRC, KC_RBRC, KC_MINUS, KC_EQUAL,                       KC_PGDN,  KC_LEFT, KC_DOWN, KC_RGHT,  KC_MINUS, KC_EQUAL, \
+  _______, XXXXXXX, KC_LGUI, KC_APP, KC_SPC, KC_UNDS,                            KC_DEL, KC_BSPC, KC_LBRC, KC_RBRC,   KC_BSLASH, _______, \
+                         _______, _______, _______, KC_LGUI ,          _______, _______, _______, _______ \
 ),
 
 /* Adjust (Lower + Raise)
