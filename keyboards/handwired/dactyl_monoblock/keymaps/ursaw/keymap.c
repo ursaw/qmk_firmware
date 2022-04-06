8a3f31e71895df04aafa80b2f2c5f47c137148a5
urs <urs.w@gmx.de>
2022-04-06 13:11:51 +0200

    dactylmono: numblock

--- keyboards/handwired/dactyl_monoblock/keymaps/ursaw/keymap.c
+++ keyboards/handwired/dactyl_monoblock/keymaps/ursaw/keymap.c
@@ -59,24 +59,24 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 ),
 
 /* Lower
- * +-----------------------------------------+      +-----------------------------------------+
- * |   ~  |   !  |   @  |   #  |   $  |   %  |      |   ^  |   &  |   *  |   (  |   )  | Bksp |
- * |------+------+------+------+------+------|      |------+------+------+------+------+------|/home/urs/qmk_firmware/keyboards/handwired/pteron/keymaps/ursaw/keymap.c
- * |   ~  |   !  |   @  |   #  |   $  |   %  |      |   ^  |   &  |   *  |   (  |   )  | Del  |
- * |------+------+------+------+------+------|      |------+------+------+------+------+------|
- * | Del  |  F1  |  F2  |  F3  |  F4  |  F5  |      |  F6  |   _  |   +  |   {  |   }  |  |   |
- * |------+------+------+------+------+------|      |------+------+------+------+------+------|
- * |      |  F7  |  F8  |  F9  |  F10 |  F11 |      |  F12 |ISO ~ |ISO | |      |      |      |
- * +-------------+------+------+------+------|      |------+------+------+------+-------------+
+ * .-----------------------------------------.                                 .-----------------------------------------.
+ * |      |      |   7  |   8  |   9  |  BS  |                                  |  Mr1 |  Mr2 | Mstp |      |      |      |
+ * |------+------+------+------+------+------|                                  |------+------+------+------+------+------|
+ * |  TAB |   *  |   4  |   5  |   6  |   +  |                                  |  M1  |  M2  |      |      |      |      |
+ * |------+------+------+------+------+------|                                  |------+------+------+------+------+------|
+ * |      |   /  |   1  |   2  |   3  |  -   |                                  |      | STOP | PREV | NEXT |      | CAPS |
+ * |------+------+------+------+------+------|                                  |------+------+------+------+------+------|
+ * |      |      |   0  |   0  |   .  |  ENT |                                  |      | Mute |  V+  | V-   |      |      |
+ * '-----------------------------------------/                                  \-----------------------------------------'
  *               |      |      |      |      |      | Next | Vol- | Vol+ | Play |
  *               +---------------------------+      +---------------------------+
  */
 [_LOWER] = LAYOUT( \
-  KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,         KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_BSPC, \
-  KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,         KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_DEL,  \
-  KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,           KC_F6,   KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE, \
-  _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,          KC_F12,S(KC_NUHS),S(KC_NUBS),KC_HOME, KC_END, _______, \
-           _______,  _______, _______, _______, _______,         KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY, _______ \
+   XXXXXXX, XXXXXXX, KC_7  , KC_8  , KC_9  , KC_BSPC,                                DM_REC1, DM_REC1, DM_RSTP, XXXXXXX, XXXXXXX, XXXXXXX,
+   _______, KC_PAST, KC_4  , KC_5  , KC_6  , KC_PLUS,                                DM_PLY1, DM_PLY2, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+   XXXXXXX ,KC_PSLS, KC_1  , KC_2  , KC_3  , KC_MINUS,                               XXXXXXX, KC_MPLY, KC_MPRV, KC_MNXT, XXXXXXX, KC_CAPS,
+   XXXXXXX, XXXXXXX, KC_0  , KC_0  ,KC_DOT , KC_ENT,                                 XXXXXXX, KC_MUTE, KC_VOLD, KC_VOLU, XXXXXXX, XXXXXXX,
+           _______,  _______, _______, _______, _______,     KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY, _______ \
 ),
 
 /* Raise
@@ -89,12 +89,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|      |------+------+------+------+------+------|
  * |      |  F7  |  F8  |  F9  |  F10 |  F11 |      |  F12 |ISO # |ISO / |      |      |      |
  * +-------------+------+------+------+------|      |------+------+------+------+-------------+
-[] *               |      |      |      |      |      | Next | Vol- | Vol+ | Play |
+ *               |      |      |      |      |      | Next | Vol- | Vol+ | Play |
  *               +---------------------------+      +---------------------------+
  */
 [_RAISE] = LAYOUT( \
   _______,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                           KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F11,\
-  _______,  KC_LGUI,  KC_LCBR,   KC_RCBR,  KC_PLUS, KC_GRV,                       KC_PGUP, KC_HOME,  KC_UP, KC_END   ,XXXXXXX, KC_F12, \
+  _______,  SELWORD,  KC_LCBR,   KC_RCBR,  KC_PLUS, KC_GRV,                       KC_PGUP, KC_HOME,  KC_UP, KC_END   ,XXXXXXX, KC_F12, \
   _______,  KC_PIPE,  KC_LBRC, KC_RBRC, KC_MINUS, KC_EQUAL,                       KC_PGDN,  KC_LEFT, KC_DOWN, KC_RGHT,  KC_MINUS, KC_EQUAL, \
   _______, XXXXXXX, KC_LGUI, KC_APP, KC_SPC, KC_UNDS,                            KC_DEL, KC_BSPC, KC_LBRC, KC_RBRC,   KC_BSLASH, _______, \
                    _______,        _______, _______, _______, KC_LGUI ,          _______, _______, _______, _______ , _______ \
