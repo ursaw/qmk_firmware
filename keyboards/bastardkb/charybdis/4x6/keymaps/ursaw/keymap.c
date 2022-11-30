380d21d12fae33d1a6a6d2aa4d7c54b9eab47416
urs <urs.w@gmx.de>
2022-11-30 14:51:20 +0100

    charybis: rearrange mouse

--- keyboards/bastardkb/charybdis/4x6/keymaps/ursaw/keymap.c
+++ keyboards/bastardkb/charybdis/4x6/keymaps/ursaw/keymap.c
@@ -56,7 +56,7 @@ enum userspace_keycodes {
 #define SPC_RSE LT(_RAISE, KC_SPC)
 #define SPC_LWR LT(_LOWER, KC_SPC)
 #define ENT_TEX LT(_LATEX, KC_ENT)
-
+#define ENT_LWR LT(_LOWER, KC_ENT)
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_QWERTY] = LAYOUT_charybdis_4x6(
@@ -65,7 +65,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    KC_TAB  , KC_Q   , KC_W   , KC_E   , KC_R   , KC_T   ,                                     KC_Y   , KC_U   , KC_I   , KC_O   , KC_P   , KC_BSPC,
    KC_LSFT , KC_A   , KC_S   , KC_D   , KC_F   , KC_G   ,                                     KC_H   , KC_J   , KC_K   , KC_L   , KC_SCLN, KC_QUOT,
    KC_LCTRL, KC_Z   , KC_X   , KC_C   , KC_V   , KC_B   ,                                     KC_N   , KC_M   , KC_COMM, KC_DOT , KC_SLSH, KC_RSFT,
-                                                RAISE  , KC_ENT, ENT_TEX ,     SPC_LWR , SPC_RSE,
+                                                RAISE  , KC_ENT, SPC_LWR ,     SPC_LWR , SPC_RSE,
                                                          KC_LALT, KC_LGUI,   KC_RALT
   // ╰──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────╯
   //                                 KC_LGUI, KC_SPC,   LOWER,      RAISE,  KC_ENT,
@@ -88,8 +88,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    */
    XXXXXXX, XXXXXXX, KC_7  , KC_8  , KC_9  , KC_BSPC,                               DM_REC1, DM_REC1, DM_RSTP, XXXXXXX, XXXXXXX, XXXXXXX,
    _______, KC_PAST, KC_4  , KC_5  , KC_6  , KC_PLUS,                                DM_PLY1, DM_PLY2, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-   XXXXXXX ,KC_PSLS, KC_1  , KC_2  , KC_3  , KC_MINUS,               POINTER_DEFAULT_DPI_FORWARD , KC_MPLY, KC_MPRV, KC_MNXT, XXXXXXX, KC_CAPS,
-   XXXXXXX, XXXXXXX, KC_0  , KC_0  ,KC_DOT , KC_ENT,                 POINTER_DEFAULT_DPI_REVERSE , KC_MUTE, KC_VOLD, KC_VOLU, XXXXXXX, XXXXXXX,
+   XXXXXXX ,KC_PSLS, KC_1  , KC_2  , KC_3  , KC_MINUS,/*POINTER_DEFAULT_DPI_FORWARD*/XXXXXXX, KC_MPLY, KC_MPRV, KC_MNXT, XXXXXXX, KC_CAPS,
+   XXXXXXX, XXXXXXX, KC_0  , KC_0  ,KC_DOT , KC_ENT,  /*POINTER_DEFAULT_DPI_REVERSE*/XXXXXXX, KC_MUTE, KC_VOLD, KC_VOLU, XXXXXXX, XXXXXXX,
                                                 _______, _______, _______,      _______, _______,
                                                          _______, _______,      _______
 
@@ -132,9 +132,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    _______, KC_F1  , KC_F2  , KC_F3  , KC_F4   , KC_F5   ,                                 KC_F6  , KC_F7  , KC_F8  , KC_F9  , KC_F10  , KC_F11  ,
    _______, SELWORD, KC_LCBR, KC_RCBR, KC_PLUS , KC_GRV  ,                                 KC_PGUP, KC_HOME, KC_UP  , KC_END , KC_INS  , KC_F12  ,
    _______, KC_PIPE, KC_LBRC, KC_RBRC, KC_MINUS, KC_EQUAL,                                 KC_PGDN, KC_LEFT, KC_DOWN, KC_RGHT, KC_MINUS, KC_EQUAL,
-   _______, KC_UNDO, KC_LGUI, KC_APP , KC_SPC  , KC_UNDS ,                                 KC_DEL , KC_BSPC, KC_LBRC, KC_RBRC, KC_BSLASH, _______,
-                                  _______, _______, KC_SPC,    KC_MS_BTN3, KC_MS_BTN1,
-                                           _______, _______,   KC_MS_BTN2
+   _______, KC_UNDO, KC_LGUI, KC_APP , KC_SPC  , KC_UNDS ,                                 KC_BSPC,KC_MS_BTN1,KC_MS_BTN3,KC_MS_BTN2, KC_BSLASH, _______,
+                                  _______, _______, KC_SPC,    _______, KC_MS_BTN1,
+                                           _______, _______,   _______
   //                            ╰───────────────────────────╯ ╰──────────────────╯
   ),
     [_LATEX] = LAYOUT_charybdis_4x6(
@@ -173,10 +173,11 @@ bool process_record_user(uint16_t keycode, keyrecord_t* record) {
 
 report_mouse_t pointing_device_task_user(report_mouse_t mouse_report) {
 
-    if (get_mods() & MOD_BIT(KC_LCTRL)) {
-        charybdis_set_pointer_dragscroll_enabled(1);
-    }else{
+    // if ( get_mods() & MOD_MASK_SHIFT ) {
+    if(biton32(layer_state)== _RAISE) {
         charybdis_set_pointer_dragscroll_enabled(0);
+    }else{
+        charybdis_set_pointer_dragscroll_enabled(1);
     }
     return mouse_report;
 }
