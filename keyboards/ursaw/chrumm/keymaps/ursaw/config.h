341d85cc155013089a1a4b53782580492cc53b1c
Urs Wihlfahrt <urs.w@gmx.de>
2024-10-03 15:19:50 +0200

    chrumm: CAPS WORD

---
+++ keyboards/ursaw/chrumm/keymaps/ursaw/config.h
@@ -0,0 +1,10 @@
+// #define TAP_CODE_DELAY 50
+// #define TAPPING_TERM 200
+// #define HOLD_ON_OTHER_KEY_PRESS
+// #define TAPPING_FORCE_HOLD
+// #define QUICK_TAP_TERM 0
+// 
+// // Mouse definitions
+// #define  MK_COMBINED
+// #define MOUSEKEY_TIME_TO_MAX  60
+#define BOTH_SHIFTS_TURNS_ON_CAPS_WORD
